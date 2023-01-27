ccbdfeb07625060a2be7609d5a90473e08696cee
Flipez <code@brauser.io>
2023-01-28 00:06:56 +0100

    add "safe" printing
    Signed-off-by: Flipez <code@brauser.io>

--- src/main.rs
+++ src/main.rs
@@ -5,27 +5,18 @@ use core::panic::PanicInfo;
 
 mod vga_buffer;
 
-static HELLO: &[u8] = b"Hello World!";
-
 #[no_mangle] // disable name mangling of this function
 pub extern "C" fn _start() -> ! {
     // This function is the entry point, since the linker looks for a function
     // named '_start' by default.
-
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    println!("Hello World{}", "!");
 
     loop {}
 }
 
 // Called on panic
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
