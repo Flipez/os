5151f0e75aaef677bd684c2daf1205a7c7154ee2
Flipez <code@brauser.io>
2024-12-23 16:21:25 +0100

    chore(os): Add test suite
    Signed-off-by: Flipez <code@brauser.io>

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 #![no_std]  // do not link the rust stdlib
 #![no_main] // disable rust entry points
+#![feature(custom_test_frameworks)]
+#![test_runner(os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-
-mod vga_buffer;
+use os::println;
 
 #[no_mangle] // disable name mangling of this function
 pub extern "C" fn _start() -> ! {
@@ -11,12 +13,27 @@ pub extern "C" fn _start() -> ! {
     // named '_start' by default.
     println!("Hello World{}", "!");
 
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
 
 // Called on panic
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    os::test_panic_handler(info)
+}
+
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
 }
