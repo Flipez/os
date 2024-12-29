38cf946ccb2a8d5b28d6b7c51fee46508ddfa350
Flipez <code@brauser.io>
2024-12-29 13:14:24 +0100

    chore(os): Add handler for double fault exceptions
    Signed-off-by: Flipez <code@brauser.io>

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,7 @@ use core::panic::PanicInfo;
 pub mod serial;
 pub mod vga_buffer;
 pub mod interrupts;
+pub mod gdt;
 
 pub trait Testable {
     fn run(&self) -> ();
@@ -73,5 +74,6 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
 }
 
 pub fn init() {
+    gdt::init();
     interrupts::init_idt();
 }
